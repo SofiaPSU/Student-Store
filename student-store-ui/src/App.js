3ad19f2df88d1abebe9fddb292ea5f1ddf9a06a3
SofiaPSU <sxr5616@psu.edu>
2021-06-18 02:31:20 -0400

    Added images to page

--- student-store-ui/src/App.js
+++ student-store-ui/src/App.js
@@ -1,42 +1,40 @@
 
 import './App.css';
-import { BrowserRouter } from "react-router-dom"
+/*import { BrowserRouter } from "react-router-dom"
 import { Routes } from "react-router"
-import { Route } from "react-router"
+import { Route } from "react-router"*/
 import { useEffect } from "react"
 import axios from "axios"
 import Home from "./Home/Home"
 import { useState } from "react"
-import Products from "./Products/Products"
+//import Products from "./Products/Products"
 import Navbar from "./NavBar/Navbar"
-import ProductDetail from "./ProductDetail/ProductDetail"
+//import ProductDetail from "./ProductDetail/ProductDetail"
 
 
 function App() {
   //useEffect function
   var [products, setProducts] = useState([])
-  useEffect(()=>{
-    const onStart = async() =>{
+  useEffect(() => {
+    const onStart = async() => {
       try{
-        const productsRequest = await axios.get("https://localhost:3000/products")
-        if(productsRequest.data){
-          setProducts(productsRequest.data)
+        const res = await axios.get("http://localhost:3001/store/products")
+        console.log("product list",res.data.products)
+        const products = res?.data?.products
+        if(products){
+          setProducts(products)
         }
       }catch(err){
-        console.log(err)
+        console.log({ err })
       }
     }
-    onStart();
+    onStart()
   }, [])
 
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-      </BrowserRouter>
+     <Navbar />
+    <Home products={products} />
     </div>
   );
 }
--- student-store-ui/src/Home/Home.js
+++ student-store-ui/src/Home/Home.js
@@ -1,12 +1,22 @@
 
 import "./Home.css"
-import Products from "../Products/Products"
+//import Products from "../Products/Products"
 
 
-export default function Home() {
+export default function Home({ products = [] }) {
   return (
     <div className="Home">
-        <Products />
+       <div className = "homepg">
+        {products.map((product) => (
+          <div className="products-preview">
+            {product.name}
+            {product.category}
+            <img src={product.image} alt = {product.name}/>
+          
+          </div>
+        ))}
+       </div>
+
     </div>
   )
 }
